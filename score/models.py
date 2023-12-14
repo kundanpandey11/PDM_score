3267b2c316caf26647f30bc55152eb03b374f3d0
kundanpandey11 <kundan.k.pandey02@gmail.com>
2023-12-14 15:08:36 +0530

    first commit

---
+++ score/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class score(models.Model):
+    #weights 
+    w1 = models.FloatField(null=True,blank=True)
+    w2 = models.FloatField(null=True,blank=True)
+    w3 = models.FloatField(null=True,blank=True)
+    
+    pdm_score = models.FloatField(null=False,blank=False)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.created_datetime} | {self.pdm_score}"
+        
